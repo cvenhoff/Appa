e689dda9784141a75fc8debe42e2cb805177f320
cvenhoff <venhoff00@gmail.com>
2023-04-26 19:14:19 +0200

    fix

--- main.py
+++ main.py
@@ -10,7 +10,4 @@ os.system('sudo ifconfig can0 up')
 
 # start the process_messages and receive_messages functions as separate threads
 mqtt_utils.connect()
-t1 = threading.Thread(target=write.process_messages)
-t2 = threading.Thread(target=write.receive_messages)
-t1.start()
-t2.start()
+write.get_messages()
--- write.py
+++ write.py
@@ -25,40 +25,28 @@ dbc = cantools.database.load_file(dbc_file)
 # initialize a queue to store incoming CAN messages
 message_queue = queue.Queue()
 
-def process_messages():
+def get_messages():
     while True: 
-        print("alive1")
-        # retrieve the next message from the queue  
-      
-        msg = message_queue.get()
-
-        # decode the message using the dbc file
-        print(msg.arbitration_id)
-        print(msg.data)
-        #decoded_message = dbc.decode_message(msg.arbitration_id+1,msg.data)
-
-        #for name in decoded_message:
-        #    # send the decoded message as a MQTT message
-        #    mqtt_utils.publish(name,decoded_message[name])
-        #    # write in csv
-        #    with open(filename, 'a', newline='') as csvfile:
-        #        t = datetime.datetime.now()
-        #        t = t.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
-        #        writer = csv.writer(csvfile)
-        #        writer.writerow(["","",0,t,t,t,decoded_message[name],name,"mqqt_consumer","local","data"])
-
-            
-def receive_messages():
-    while True:
-        print("alive")
         # receive a message from the CAN bus
         try:
-            continue
-            #msg = bus.recv()
-            #if(msg != None):
-                # put the message in the queue for processing
-            #    message_queue.put(msg)
+            msg = bus.recv()
+            if(msg != None):
+                # decode the message using the dbc file
+                print(msg.arbitration_id)
+                print(msg.data)
+                #decoded_message = dbc.decode_message(msg.arbitration_id+1,msg.data)
+
+                #for name in decoded_message:
+                #    # send the decoded message as a MQTT message
+                #    mqtt_utils.publish(name,decoded_message[name])
+                #    # write in csv
+                #    with open(filename, 'a', newline='') as csvfile:
+                #        t = datetime.datetime.now()
+                #        t = t.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
+                #        writer = csv.writer(csvfile)
+                #        writer.writerow(["","",0,t,t,t,decoded_message[name],name,"mqqt_consumer","local","data"])
+
         except Exception as e:
             print(str(e))
             continue
-
+      
