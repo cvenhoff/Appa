7049f5ece16bf0d4bfa254700a2acac771bdfed6
cvenhoff <venhoff00@gmail.com>
2023-04-26 20:00:02 +0200

    fix

--- write.py
+++ write.py
@@ -27,24 +27,25 @@ message_queue = queue.Queue()
 
 def process_messages():
     while True: 
+        try:
+            # retrieve the next message from the queue  
+            msg = message_queue.get()
 
-        # retrieve the next message from the queue  
-      
-        msg = message_queue.get()
+            # decode the message using the dbc file
+            decoded_message = dbc.decode_message(msg.arbitration_id,msg.data)
 
-        # decode the message using the dbc file
-        decoded_message = dbc.decode_message(msg.arbitration_id,msg.data)
-
-        for name in decoded_message:
-            # send the decoded message as a MQTT message
-            mqtt_utils.publish(name,decoded_message[name])
-        #    # write in csv
-        #    with open(filename, 'a', newline='') as csvfile:
-        #        t = datetime.datetime.now()
-        #        t = t.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
-        #        writer = csv.writer(csvfile)
-        #        writer.writerow(["","",0,t,t,t,decoded_message[name],name,"mqqt_consumer","local","data"])
-
+            for name in decoded_message:
+                # send the decoded message as a MQTT message
+                mqtt_utils.publish(name,decoded_message[name])
+                # write in csv
+                with open(filename, 'a', newline='') as csvfile:
+                    t = datetime.datetime.now()
+                    t = t.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
+                    writer = csv.writer(csvfile)
+                    writer.writerow(["","",0,t,t,t,decoded_message[name],name,"mqqt_consumer","local","data"])
+        except Exception as e:
+            print(e)
+            continue
             
 def receive_messages():
     while True:
@@ -53,7 +54,7 @@ def receive_messages():
             msg = bus.recv(10.0)
             if(msg != None):
                 # put the message in the queue for processing
-                if(int(msg.arbitration_id) != 0): message_queue.put(msg)
+                message_queue.put(msg)
         except Exception as e:
             print(str(e))
             continue
