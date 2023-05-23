4e5fef15fe0d631cccdc71127704b6a939a0eeb7
cvenhoff <venhoff00@gmail.com>
2023-05-23 08:56:16 +0200

    dbc file update

--- csv_utils.py
+++ csv_utils.py
@@ -1,16 +1,23 @@
 import csv
+import datetime
 
 def init_csv(filename):
     datatypes = ["#datatype","string","long","dateTime:RFC3339","dateTime:RFC3339","dateTime:RFC3339","double","string","string","string","string"]
     init = ["#default","mean","","","","","","","","",""]
     flags = ["#group","false","false","true","true","false","false","true","true","true","true"]
     header = ["",'result', 'table', '_start', '_stop', '_time', '_value', '_field', '_measurement', 'host', 'topic']
+
+    with open(filename, 'a', newline='') as csvfile:
+        t = datetime.datetime.now()
+        t = t.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
+        writer = csv.writer(csvfile)
+        writer.writerow(["","",0,t,t,t,"","","NEW RUN","",""])
     # Write header to file
-    with open(filename, 'r+', newline='') as f:
-        f.seek(0)
-        writer = csv.writer(f)
-        writer.writerow(datatypes)
-        writer.writerow(init)
-        writer.writerow(flags)
-        writer.writerow(header)
-        f.seek(0, 2)
+    # with open(filename, 'r+', newline='') as f:
+    #    f.seek(0)
+    #    writer = csv.writer(f)
+    #    writer.writerow(datatypes)
+    #    writer.writerow(init)
+    #    writer.writerow(flags)
+    #    writer.writerow(header)
+    #    f.seek(0, 2)
